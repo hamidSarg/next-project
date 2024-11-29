e9c2ccf57df5383aebaf7e38fe3cbcc18da22c11
Hamid <hamid@hamsaa.ir>
2024-11-29 04:26:12 +0330

    feat: defines fonts and font sizes and animation key frame to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,31 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      'regular-12-500': ['12px', {
+        fontWeight: '500',
+        lineHeight: '20px',
+      }],
+      'regular-14-400': ['14px', {
+        lineHeight: '114.2%',
+        fontWeight: '400',
+      }],
+    },
     extend: {
+      fontFamily: {
+        primary: 'IRANSansX',
+        primaryBold: 'IRANSansX-Bold'
+      },
+      animation: {
+        'ping-pong': 'pingPong 10s linear infinite',
+      },
+      keyframes: {
+        pingPong: {
+          '0%': { transform: 'translateX(0%)' },
+          '50%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
