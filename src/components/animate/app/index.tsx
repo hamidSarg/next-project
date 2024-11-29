60073d27e04e8db972eb9caaea73508dd2f81b50
Hamid <hamid@hamsaa.ir>
2024-11-29 04:31:18 +0330

    feat: app backGround animation Component

---
+++ src/components/animate/app/index.tsx
@@ -0,0 +1,8 @@
+
+
+
+export const AnimatedFlash = () => {
+    return (
+        <div className="w-[400px] h-[400px] bg-blue-900 opacity-50 shadow-[0_150px_250px_rgba(0,0,139,0.5)] blur-3xl animate-ping-pong absolute top-[200px]" />
+    );
+};
